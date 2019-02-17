4fe7e2e4d80a8fbed8c2b7f9c6b9f495885a7876
Serj <sgtserj94@gmail.com>
2019-02-17 12:47:43 +0900

    Updates

---
+++ precache-manifest.af8ba727773bdf10d4748b88275b6e79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a94f716bb1b3b3e02b38",
+    "url": "/website/static/css/main.b4e7e306.chunk.css"
+  },
+  {
+    "revision": "a94f716bb1b3b3e02b38",
+    "url": "/website/static/js/main.a94f716b.chunk.js"
+  },
+  {
+    "revision": "738c5e760cdb5d1c1801",
+    "url": "/website/static/js/runtime~main.738c5e76.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/website/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9a49bd686db73fb91987f9ca360d5155",
+    "url": "/website/index.html"
+  }
+];
